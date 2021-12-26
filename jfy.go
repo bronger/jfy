5f4108e59b289555d0fac418f4a8932a9845166f
Torsten Bronger <bronger@physik.rwth-aachen.de>
2021-12-26 19:25:14 +0100

    Implement signal and output pipe handling

--- jfy.go
+++ jfy.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	go func() {
+		signals := make(chan os.Signal, 32)
+		signal.Notify(signals)
+		for cmd.ProcessState == nil {
+			sig := <-signals
+			for cmd.Process == nil {
+				time.Sleep(10 * time.Millisecond)
+			}
+			cmd.Process.Signal(sig)
+		}
+		signal.Reset()
+	}()
 	cmd.Stdin = os.Stdin
-	out, err := cmd.CombinedOutput()
-	if err != nil {
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+	if err := cmd.Wait(); err != nil {
 		if ee, ok := err.(*exec.ExitError); ok {
 			os.Exit(ee.ProcessState.ExitCode())
 		} else {
 			panic(err)
 		}
 	}
-	fmt.Printf("%s", out)
+	stdout := stdoutBuf.Bytes()
+	//	stderr := stderrBuf.Bytes()
+	fmt.Printf("%s", stdout)
 }
